3ddf82c99d90844af3718ee02107b0c4b3ee38fa
Jhonny Martinez <jhonny9550@gmail.com>
2023-05-06 14:09:17 +1000

    feat: fixed api call request header

--- client/service/api.service.js
+++ client/service/api.service.js
@@ -4,14 +4,18 @@ const ISSUES_ENDPOINT = 'issues';
 
 // Handle HTTP request
 const makeRequest = (method, id, body) => {
+    console.log(JSON.stringify(body));
     let url = `${SERVER_URL}:${SERVER_PORT}/${ISSUES_ENDPOINT}`;
     if (id) {
         url = `${url}/${id}`;
     }
     return fetch(url, {
         method,
-        body: JSON.stringify(body)
-    }).then(res => res.json())
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json());
 }
 
 module.exports = { makeRequest };
